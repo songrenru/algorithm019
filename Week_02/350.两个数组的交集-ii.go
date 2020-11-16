1bcc09df553e47d7cda5e2fc830d4b225919b636
songrr <164354835@qq.com>
2020-11-16 15:47:26 +0800

    ..

--- Week_02/350.两个数组的交集-ii.go
+++ Week_02/350.两个数组的交集-ii.go
@@ -6,18 +6,39 @@
 
 // @lc code=start
 func intersect(nums1 []int, nums2 []int) []int {
-	if len(nums1) > len(nums2) {
-		return intersect(nums2, nums1)
-	}
-	map1 := make(map[int]int)
-	for _, v := range nums1 {
-		map1[v]++
-	}
+	// // hash
+	// if len(nums1) > len(nums2) {
+	// 	return intersect(nums2, nums1)
+	// }
+	// map1 := make(map[int]int)
+	// for _, v := range nums1 {
+	// 	map1[v]++
+	// }
+	// intersection := []int{}
+	// for _, v := range nums2 {
+	// 	if map1[v] > 0 {
+	// 		intersection = append(intersection, v)
+	// 		map1[v]--
+	// 	}
+	// }
+	// return intersection
+
+	// sort
+	sort.Ints(nums1)
+	sort.Ints(nums2)
+	
 	intersection := []int{}
-	for _, v := range nums2 {
-		if map1[v] > 0 {
-			intersection = append(intersection, v)
-			map1[v]--
+	i, lens1 := 0, len(nums1)
+	j, lens2 := 0, len(nums2)
+	for i < lens1 && j < lens2 {
+		if nums1[i] == nums2[j] {
+			intersection = append(intersection, nums1[i])
+			i++
+			j++
+		} else if nums1[i] < nums2[j] {
+			i++
+		} else {
+			j++
 		}
 	}
 	return intersection
